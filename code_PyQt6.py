3def176ec7b3791f487ef0dc6365d3b604b24aec
AnnaKhohlova <anna0106cat@gmail.com>
2025-10-26 09:23:09 +0700

    Update code_PyQt6.py

--- code_PyQt6.py
+++ code_PyQt6.py
@@ -11,8 +11,8 @@ class MainWindow(QWidget):
 
     def initializeUI(self):
         """Set up the application's GUI."""
-        self.setGeometry(50, 50, 250, 400)
-        self.setWindowTitle("2.1 - User Profile GUI")
+        self.setGeometry(50, 50, 250, 500)
+        self.setWindowTitle("2.1 - Just a cute kitty")
 
         self.setUpMainWindow()
         self.show()
@@ -22,57 +22,56 @@ class MainWindow(QWidget):
         self.createImageLabels()
 
         user_label = QLabel(self)
-        user_label.setText("John Doe")
+        user_label.setText("Michelle")
         user_label.setFont(QFont("Arial", 20))
-        user_label.move(85, 140)
+        user_label.move(85, 230)
 
         bio_label = QLabel(self)
         bio_label.setText("Biography")
         bio_label.setFont(QFont("Arial", 17))
-        bio_label.move(15, 170)
+        bio_label.move(15, 260)
 
         about_label = QLabel(self)
-        about_label.setText("I'm a Software Engineer with 10 years\
-            experience creating awesome code.")
+        about_label.setText("I'm still a little fluffy kitten " \
+        "who wants to become a big cat. I have a lot of " \
+        "experience playing with balls and tыgыdыk.")
         about_label.setWordWrap(True)
-        about_label.move(15, 190)
+        about_label.move(15, 290)
 
         skills_label = QLabel(self)
         skills_label.setText("Skills")
         skills_label.setFont(QFont('Arial', 17))
-        skills_label.move(15, 240)
+        skills_label.move(15, 350)
 
         languages_label = QLabel(self)
-        languages_label.setText("Python  |  PHP  |  SQL  |  JavaScript")
-        languages_label.move(15, 260)
+        languages_label.setText("I touch everyone around me." \
+        "I can scream Meow for a long time, "
+        "especially at night.")
+        languages_label.move(15, 375)
 
         experience_label = QLabel(self)
         experience_label.setText("Experience")
         experience_label.setFont(QFont("Arial", 17))
-        experience_label.move(15, 290)
-
-        developer_label = QLabel(self)
-        developer_label.setText("Python Developer")
-        developer_label.move(15, 310)
+        experience_label.move(15, 400)
 
         dev_dates_label = QLabel(self)
-        dev_dates_label.setText("Mar 2011 - Present")
+        dev_dates_label.setText("Sofa Ripper")
         dev_dates_label.setFont(QFont("Arial", 10))
-        dev_dates_label.move(15, 330)
+        dev_dates_label.move(15, 430)
 
         driver_label = QLabel(self)
-        driver_label.setText("Pizza Delivery Driver")
-        driver_label.move(15, 350)
+        driver_label.setText("The flower eater")
+        driver_label.move(15, 450)
 
         driver_dates_label = QLabel(self)
-        driver_dates_label.setText("Aug 2015 - Dec 2017")
+        driver_dates_label.setText("A broken vase and your heart")
         driver_dates_label.setFont(QFont("Arial", 10))
-        driver_dates_label.move(15, 370)
+        driver_dates_label.move(15, 470)
 
     def createImageLabels(self):
         """Open image files and create image labels."""
         images = ["images/skyblue.png", 
-                  "images/profile_image.png"]
+                  "images/Kotik.png"]
 
         for image in images:
             try:
@@ -80,8 +79,8 @@ class MainWindow(QWidget):
                     label = QLabel(self)
                     pixmap = QPixmap(image)
                     label.setPixmap(pixmap)
-                    if image == "images/profile_image.png":
-                        label.move(80, 20) 
+                    if image == "images/Kotik.png":
+                        label.move(25, 20) 
             except FileNotFoundError as error:
                 print(f"Image not found.\nError: {error}")            
 
